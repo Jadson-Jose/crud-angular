7bb887888dd5e40729b28585b2b329be6044e7cb
Jadson-Jose <jadsonjosesilva@gmail.com>
2021-10-05 17:43:58 -0300

    cont

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,18 +1,18 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
 
+import { AppMaterialModule } from './../shared/app-material/app-material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
+
 
 @NgModule({
   declarations: [
     CoursesComponent
   ],
   imports: [
-    CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    AppMaterialModule
+
   ]
 })
 export class CoursesModule { }
